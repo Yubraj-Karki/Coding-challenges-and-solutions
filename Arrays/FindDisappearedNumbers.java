dffbd6db6f3dc5974ae218683f85b9c380ee3f6d
YubrajKarki <yubrajkarki528@gmail.com>
2022-06-02 23:18:38 +0545

    Find disappeared numbers problem

--- Arrays/FindDisappearedNumbers.java
+++ Arrays/FindDisappearedNumbers.java
@@ -1,38 +1,40 @@
 package Arrays;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
-public class MissingNumber {
+public class FindDisappearedNumbers {
     public static void main(String[] args) {
+        int array[] = { 6, 1, 2, 3, 4, 10, 5, 7 };
 
-        // int array[] = { 6, 1, 2, 8, 3, 4, 7, 10, 5 };
-        int array[] = { 6, 1, 2, 8, 3, 4, 10, 5, 9 };
-
-        System.out.println(findNumber(array));
-
-        System.out.println(Arrays.toString(array));
+        findDisappearedNumbers(array);
     }
 
-    public static int findNumber(int[] arr) {
-        int ans = -1;
+    public static ArrayList<Integer> findDisappearedNumbers(int[] arr) {
+
         int i = 0;
         while (i < arr.length) {
             int correctIndex = arr[i] - 1;
             if (arr[i] < arr.length && arr[i] != arr[correctIndex]) {
                 swap(arr, i, correctIndex);
             } else {
+
                 i++;
             }
         }
 
+        ArrayList<Integer> ans = new ArrayList<>();
+
         // Finding the missing element
         for (int j = 0; j < arr.length; j++) {
             if (arr[j] != j + 1) {
-                return ans = j + 1;
+                ans.add(j + 1);
             }
         }
+        System.out.println(Arrays.toString(ans.toArray()));
 
-        return arr.length;
+        return ans;
+
     }
 
     private static void swap(int[] arr, int i, int correctIndex) {
@@ -42,5 +44,4 @@ public class MissingNumber {
         arr[correctIndex] = temp;
 
     }
-
 }
